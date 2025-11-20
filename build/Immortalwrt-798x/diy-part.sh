8cb8b5a7a777b0d30c48c7fce79c66b308dd3b4d
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-20 09:38:03 +0800

    Update diy-part.sh

--- build/Immortalwrt-798x/diy-part.sh
+++ build/Immortalwrt-798x/diy-part.sh
@@ -4,9 +4,9 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-echo 'src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus' >>feeds.conf.default
+echo "src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus.git;main" >> "feeds.conf.default"
+echo 'src-git kucat https://gitclone.com/github.com/sirpdboy/luci-theme-kucat.git;master' >>feeds.conf.default
 
-echo 'src-git kucat https://github.com/sirpdboy/luci-theme-kucat' >>feeds.conf.default
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.99.1"            # 修改openwrt后台地址(填0为关闭)
