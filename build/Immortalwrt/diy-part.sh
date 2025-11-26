85f54081f0840120af623bc903c4ea60801adf7e
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-26 08:44:27 +0800

    更新 diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -4,8 +4,9 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-# git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+
 git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+git clone https://github.com/sirpdboy/luci-app-kucat-config package/luci-theme-kucat-config
 
 
 # 后台IP设置
