8a6a07b18850931849151879f58369f52609bc3e
shine85 <73932625+shine85@users.noreply.github.com>
2026-01-05 14:01:30 +0800

    更新 diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -98,6 +98,8 @@ EOF
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
 cat >>$DELETE <<-EOF
 EOF
+
+
 # --- 修复 Kucat 配置插件不显示的问题 ---
 # 强制开启依赖库 (这是最关键的一步！)
 echo "CONFIG_PACKAGE_luci-compat=y" >> .config
